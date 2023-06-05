ab9ac1f829024a84fc19be314a3318b6d8fede1f
María José Bogantes Murillo <88169247+mboga09@users.noreply.github.com>
2023-06-05 07:25:57 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import "./style.css";
+
+window.onload = () => {
+  let randomNumber = Math.random() *10;
+  console.log('hola' + randomNumber);
+};
--- src/app.js
+++ src/app.js
@@ -1,11 +1,70 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  console.log("hola");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["A", "The", "My"];
+  let subject = [
+    "dog",
+    "cat",
+    "racoon",
+    "mouse",
+    "brother",
+    "yigüirro",
+    "monkey",
+    "cow"
+  ];
+  let action = [
+    "ate my",
+    "threw away my",
+    "yelled at my",
+    "took my",
+    "stole my",
+    "destroyed my",
+    "attacked my"
+  ];
+  let avoid = [
+    "homework",
+    "train ticket",
+    "computer",
+    "phone",
+    "car",
+    "shoes",
+    "food",
+    "calculator",
+    "backpack"
+  ];
+  let where = [
+    "yesterday",
+    "at my house",
+    "at university",
+    "at the mall",
+    "on the street"
+  ];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subIndx = Math.floor(Math.random() * subject.length);
+  let actIndx = Math.floor(Math.random() * action.length);
+  let avoidIndx = Math.floor(Math.random() * avoid.length);
+  let whereIndx = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndx] +
+    " " +
+    subject[subIndx] +
+    " " +
+    action[actIndx] +
+    " " +
+    avoid[avoidIndx] +
+    " " +
+    where[whereIndx]
+  );
 };
